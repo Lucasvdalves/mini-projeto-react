88f02cdb99de84e71495e0f8c8827a805bed2d71
Lucas Damasceno <lucasalvesbh2018@gmail.com>
2024-03-15 12:13:15 -0300

    primeiro commit

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const AuthContext = createContext();
+const AuthContext = createContext ();
 
 export const useAuth = () => useContext(AuthContext);
 
